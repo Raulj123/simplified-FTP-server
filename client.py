74f9a30c24b76d4b97719060e4041be861b413be
raulj <112786129+Raulj123@users.noreply.github.com>
2024-02-28 11:53:35 -0800

    get command done

--- client.py
+++ client.py
@@ -12,3 +12,68 @@ else:
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((server, port))
 
+# funtion to avoid overflow 
+def recvAll(sock, numBytes):
+
+	# The buffer
+	recvBuff = ""
+	
+	# The temporary buffer
+	tmpBuff = ""
+	
+	# Keep receiving till all is received
+	while len(recvBuff) < numBytes:
+		
+		# Attempt to receive bytes
+		tmpBuff = sock.recv(numBytes)
+		
+		# The other side has closed the socket
+		if not tmpBuff:
+			break
+		
+		# Add the received bytes to the buffer
+		recvBuff += tmpBuff.decode()
+	
+	return recvBuff
+
+
+
+# main loop 
+while True:
+    user_choice = input("ftp> ")
+    user_list = user_choice.split()
+
+    if user_choice.startswith("get") and len(user_list) == 2:
+        # send data to get file 
+        clientSocket.send(user_choice.encode())
+
+        fileData = ""                   # buffer to store all data
+        fileSize = 0                    # Size of incomming file
+        fileSizeBuff = ""               # Buffer with file size
+
+        fileSizeBuff = recvAll(clientSocket, 10)        # first 10 bytes contain file size
+        
+        if fileSizeBuff.count("0") == 10:
+             print("File does not exist \n")
+        else:
+             fileSize = int(fileSizeBuff)
+             print(f"File size is {fileSize} \n")
+
+             fileData = recvAll(clientSocket, fileSize)     # contains rest of file
+             print(f"File data: \n {fileData} \n")
+
+
+    
+    elif user_choice.startswith("ls"):
+        clientSocket.send("ls".encode())
+
+    elif user_choice.startswith("put"):
+        clientSocket.send(user_choice.encode())
+
+    elif user_choice.startswith("exit"):
+        break
+    else:
+        print("Invalid choice")
+
+print("closing socket")
+clientSocket.close()
--- server.py
+++ server.py
@@ -14,7 +14,53 @@ else:
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', port))
 serverSocket.listen(5)
-print("Server ready")
+print("Server ready \n")
 
 conn, addy = serverSocket.accept()
-print(f"Client connection at socket: {addy[1]}")
+print(f"Client connection at socket: {addy[1]} \n")
+
+while True:
+   data = conn.recv(3000).decode()
+
+   if data.startswith("get"):
+       fileName = data[4:]
+       try:
+            with open(fileName, "r") as file:
+               numSent = 0
+               fileData = None
+
+               while True:
+                   fileData = file.read(65536)
+
+                   if fileData:
+                       size = str(len(fileData))
+                       while len(size) < 10:
+                            size = "0" + size
+                        
+                       fileData = size + fileData
+                       numSent = 0
+
+                       while len(fileData) > numSent:
+                           numSent += conn.send(fileData[numSent:].encode())
+                   else:
+                       break
+           
+            print(f"Sent file of size: {size}")
+            
+       except FileNotFoundError:
+           print("File not found")
+           noFile = "0" * 10
+           conn.send(noFile.encode())
+       
+   elif data.startswith("ls"):
+       continue
+       
+   elif data.startswith("put"):
+       continue
+
+   else:
+       print(f"Disconnected! at address: {addy}")
+       break
+
+serverSocket.close()
+        
